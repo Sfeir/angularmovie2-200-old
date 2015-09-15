322665c744b5ce91ac147730660e9d6d527c5e2f
cbalit <dou4872>
2015-09-15 13:28:47 +0200

    step-1-solution

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,2 +1,7 @@
 /// <reference path="../lib/tsd.d.ts" />
+
 import {bootstrap} from 'angular2/angular2';
+import {MoviesComponent} from 'ts/components/movies/Movies';
+
+
+bootstrap(MoviesComponent);
