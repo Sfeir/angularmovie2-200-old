5a01c4e7ba582772dfa01c94f21cc535db051f8d
cbalit <dou4872>
2015-09-16 16:14:14 +0200

    step-5-solution

--- app/ts/components/movies/Movies.ts
+++ app/ts/components/movies/Movies.ts
@@ -42,4 +42,18 @@ export class MoviesComponent {
             console.log('parsing failed', ex)
         })
     }
+    deleteMovie(index,movie){
+        window.fetch('/api/movies/'+movie.id, {
+            method: 'delete',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movie)
+        }).then((response)=> {
+            this.movies.splice(index, 1);
+        }).catch(function(ex) {
+            console.log('parsing failed', ex)
+        })
+    }
 }
