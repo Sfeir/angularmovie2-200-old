d5a4a0c4c8a8e4e2c0eef3e46b97bce9ba2c8a1c
cbalit <dou4872>
2015-09-16 14:26:05 +0200

    step-4

---
+++ app/ts/app/App.ts
@@ -0,0 +1,14 @@
+import {View, Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'movies-app'
+})
+@View({
+    templateUrl:'ts/app/app.html',
+    directives:[]
+})
+export class App {
+    constructor() {
+
+    }
+}
