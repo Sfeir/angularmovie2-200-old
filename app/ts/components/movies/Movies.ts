61d2056d5fe7789b6a8fea8b1d7da678bd905a4e
Cyril Balit <cbalit@gmail.com>
2015-10-22 22:21:01 +0200

    use Http service for delete

--- app/ts/components/movies/Movies.ts
+++ app/ts/components/movies/Movies.ts
@@ -21,30 +21,25 @@ export class MoviesComponent {
     }
 
     getMovies() {
-            this.http.get('api/movies').map(res => res.json())
+        this.http.get('api/movies')
+            .map(res => res.json())
             .subscribe((movies)=> {
                 this.movies = movies;
             });
     }
 
     addMovie(movie) {
-        this.http.post('api/movies', JSON.stringify(movie),{headers: new Headers({'Content-Type': 'application/json'})}).map(res => res.json())
+        this.http.post('api/movies', JSON.stringify(movie), {headers: new Headers({'Content-Type': 'application/json'})})
+            .map(res => res.json())
             .subscribe((newMovie)=> {
                 this.movies.push(newMovie);
             });
     }
-    deleteMovie(index,movie){
-        window.fetch('/api/movies/'+movie.id, {
-            method: 'delete',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(movie)
-        }).then((response)=> {
-            this.movies.splice(index, 1);
-        }).catch(function(ex) {
-            console.log('parsing failed', ex)
-        })
+
+    deleteMovie(index, movie) {
+        this.http.get('/api/movies/' + movie.id)
+            .subscribe((resp)=> {
+                this.movies.splice(index, 1);
+            });
     }
 }
