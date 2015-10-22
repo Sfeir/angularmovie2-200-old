47aba20fd18f36ec5322d07e36c09aeafa086358
cbalit <dou4872>
2015-10-22 10:28:32 +0200

    upgrade Angular version

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,1 +1,5 @@
 import {bootstrap} from 'angular2/angular2';
+import {MoviesComponent} from './components/movies/Movies';
+
+
+bootstrap(MoviesComponent);
--- app/ts/bootstrapHome.ts
+++ app/ts/bootstrapHome.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from 'angular2/angular2';
-import {HomeComponent} from 'ts/components/home/Home';
+import {HomeComponent} from './components/home/Home';
 
 
 bootstrap(HomeComponent);
