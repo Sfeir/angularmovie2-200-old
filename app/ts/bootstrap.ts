f6e6316165f7233a4d4adb4dbafc3f350cab6c3d
cbalit <dou4872>
2015-10-22 10:19:39 +0200

    upgrade Angular version

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,7 +1,1 @@
-/// <reference path="../lib/tsd.d.ts" />
-
 import {bootstrap} from 'angular2/angular2';
-import {MoviesComponent} from 'ts/components/movies/Movies';
-
-
-bootstrap(MoviesComponent);
