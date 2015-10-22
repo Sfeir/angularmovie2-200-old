db087240df663b917e4519433d09e0de60e5e592
Cyril Balit <cbalit@gmail.com>
2015-10-22 22:06:58 +0200

    update to alpha-44 and new Router conf

--- app/ts/app/App.ts
+++ app/ts/app/App.ts
@@ -1,7 +1,7 @@
 import {View, Component, Inject} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router'
-import {HomeComponent} from 'ts/components/home/Home'
-import {MoviesComponent} from 'ts/components/movies/Movies'
+import {HomeComponent} from '../components/home/Home'
+import {MoviesComponent} from '../components/movies/Movies'
 
 @Component({
     selector: 'movies-app'
@@ -12,8 +12,8 @@ import {MoviesComponent} from 'ts/components/movies/Movies'
 })
 @RouteConfig([
     { path: '/', redirectTo: '/movies' },
-    { path: '/home', as: 'home', component: HomeComponent },
-    { path: '/movies', as: 'movies', component: MoviesComponent }
+    { path: '/home', as: 'Home', component: HomeComponent },
+    { path: '/movies', as: 'Movies', component: MoviesComponent }
 ])
 export class App {
     constructor(@Inject(Router)router) {
