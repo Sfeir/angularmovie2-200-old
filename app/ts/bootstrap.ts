cb2b8f8a84a666f2ab0b9a03659c14e58b7f9ef1
cbalit <dou4872>
2015-12-29 14:25:16 +0100

    step-1-solution

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,5 +1,4 @@
-import {bootstrap} from 'angular2/angular2';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {bootstrap} from 'angular2/platform/browser';
 import {MoviesComponent} from './components/movies/Movies';
 
 
