42a3b4f81a4c2231dbf2d6ad705b821bf6863703
cbalit <dou4872>
2015-12-29 15:49:25 +0100

    angular2 beta router solution

--- app/ts/app/App.ts
+++ app/ts/app/App.ts
@@ -1,24 +1,21 @@
-import {View, Component, Inject} from 'angular2/angular2';
+import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router'
 import {HomeComponent} from '../components/home/Home'
 import {MoviesComponent} from '../components/movies/Movies'
 import {EditMovieComponent} from '../components/editMovie/EditMovie'
 
 @Component({
-    selector: 'movies-app'
-})
-@View({
+    selector: 'movies-app',
     templateUrl:'ts/app/app.html',
     directives:[ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    { path: '/', redirectTo: '/movies' },
-    { path: '/home', as: 'Home', component: HomeComponent },
-    { path: '/movies', as: 'Movies', component: MoviesComponent },
-    { path: '/movies/edit/:id', as: 'Editmovie', component: EditMovieComponent }
+    { path: '/', redirectTo: ['/Movies'] },
+    { path: '/home', name: 'Home', component: HomeComponent },
+    { path: '/movies', name: 'Movies', component: MoviesComponent }
 ])
 export class App {
-    constructor(@Inject(Router)router) {
+    constructor(router:Router) {
 
     }
 }
