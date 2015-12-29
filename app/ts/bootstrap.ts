587f51f5651be670be488b1bc85120bd53d3a452
cbalit <dou4872>
2015-12-29 15:08:56 +0100

    angular 2 beta step-3-solution

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {MoviesComponent} from './components/movies/Movies';
-
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 bootstrap(MoviesComponent,[HTTP_PROVIDERS]);
