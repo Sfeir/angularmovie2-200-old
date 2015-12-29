bba0188bc576f59a137e353b805f7d7cf3f6b9a2
cbalit <dou4872>
2015-12-29 15:49:25 +0100

    angular2 beta router solution

--- app/ts/app/App.ts
+++ app/ts/app/App.ts
@@ -1,22 +1,20 @@
-import {View, Component, Inject} from 'angular2/angular2';
+import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router'
 import {HomeComponent} from '../components/home/Home'
 import {MoviesComponent} from '../components/movies/Movies'
 
 @Component({
-    selector: 'movies-app'
-})
-@View({
+    selector: 'movies-app',
     templateUrl:'ts/app/app.html',
     directives:[ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    { path: '/', redirectTo: '/movies' },
-    { path: '/home', as: 'Home', component: HomeComponent },
-    { path: '/movies', as: 'Movies', component: MoviesComponent }
+    { path: '/', redirectTo: ['/Movies'] },
+    { path: '/home', name: 'Home', component: HomeComponent },
+    { path: '/movies', name: 'Movies', component: MoviesComponent }
 ])
 export class App {
-    constructor(@Inject(Router)router) {
+    constructor(router:Router) {
 
     }
 }
--- app/ts/components/movies/Movies.ts
+++ app/ts/components/movies/Movies.ts
@@ -1,11 +1,13 @@
-import {Component, View, NgFor, Inject} from 'angular2/angular2';
-import {Http,Headers} from 'angular2/http';
+import {Component} from 'angular2/core';
+import {NgFor} from 'angular2/common';
 import {MovieFormComponent} from '../movieForm/MovieFormComponent';
+import {Http,Headers} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/interval';
 
 @Component({
-    selector: 'movies'
-})
-@View({
+    selector: 'movies',
     templateUrl: 'ts/components/movies/movies.html',
     directives: [NgFor, MovieFormComponent]
 })
@@ -14,32 +16,24 @@ export class MoviesComponent {
     movies:any;
     http:Http;
 
-    constructor(@Inject(Http)http) {
+    constructor(http:Http) {
         this.http = http;
         this.movies = [];
         this.getMovies();
     }
 
     getMovies() {
-        this.http.get('api/movies')
-            .map(res => res.json())
+        this.http.get('api/movies').map(res => res.json())
             .subscribe((movies)=> {
                 this.movies = movies;
             });
     }
 
     addMovie(movie) {
-        this.http.post('api/movies', JSON.stringify(movie), {headers: new Headers({'Content-Type': 'application/json'})})
+        this.http.post('api/movies', JSON.stringify(movie),{headers: new Headers({'Content-Type': 'application/json'})})
             .map(res => res.json())
             .subscribe((newMovie)=> {
                 this.movies.push(newMovie);
-            });
-    }
-
-    deleteMovie(index, movie) {
-        this.http.get('/api/movies/' + movie.id)
-            .subscribe((resp)=> {
-                this.movies.splice(index, 1);
             });
     }
 }
