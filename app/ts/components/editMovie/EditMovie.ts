872ceb5dc5670a0df7a2ed1cd5965f88a98bfd1c
cbalit <dou4872>
2015-12-29 16:16:52 +0100

    angular2 beta step-6-solution

--- app/ts/components/editMovie/EditMovie.ts
+++ app/ts/components/editMovie/EditMovie.ts
@@ -1,29 +1,25 @@
-import {Component, View, FORM_DIRECTIVES,CORE_DIRECTIVES, Inject, ControlGroup,FormBuilder,Validators,Control} from 'angular2/angular2';
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common';
+import {Router,RouterLink,RouteParams} from 'angular2/router'
 import {Http,Headers} from 'angular2/http';
-import {Router,RouterLink,RouteParams} from 'angular2/router';
-import {MoviesService} from '../../services/MoviesService';
-
-
 
 @Component({
-    selector: 'edit-movie'
-})
-@View({
+    selector: 'edit-movie',
     templateUrl: 'ts/components/editMovie/editMovie.html',
     directives: [CORE_DIRECTIVES,FORM_DIRECTIVES, RouterLink]
 })
 export class EditMovieComponent {
     id:string;
     router:Router;
+    http:Http;
     movie:any;
     http:Http;
     movieForm: ControlGroup;
     moviesService:MoviesService;
 
-    constructor(@Inject(Router)router, @Inject(RouteParams)routeParams,@Inject(Http)http,@Inject(FormBuilder)builder,@Inject(MoviesService)moviesService) {
+    constructor(router:Router,routeParams:RouteParams,http:Http) {
         this.router = router;
         this.http = http;
-        this.moviesService=moviesService;
         this.id = routeParams.get('id');
         this.movie = {};
         this.movieForm = builder.group(
@@ -44,61 +40,16 @@ export class EditMovieComponent {
     }
 
     getMovie(id:String) {
-        this.moviesService.getMovie(this.id)
+        this.http.get('api/movies/'+ id)
+            .map(res => res.json())
             .subscribe((movie)=> {
                 this.movie = movie;
-                this.movieForm.controls['title'].updateValue(this.movie.title);
-                this.movieForm.controls['releaseYear'].updateValue(this.movie.releaseYear);
-                this.movieForm.controls['directors'].updateValue(this.movie.directors);
-                this.movieForm.controls['actors'].updateValue(this.movie.actors);
-                this.movieForm.controls['rate'].updateValue(this.movie.rate);
             });
     }
     editMovie() {
-        this.movie.title=this.movieForm.value.title;
-        this.movie.releaseYear=this.movieForm.value.releaseYear;
-        this.movie.directors=this.movieForm.value.directors;
-        this.movie.actors=this.movieForm.value.actors;
-        this.movie.rate=this.movieForm.value.rate;
-
-
-        this.moviesService.updateMovie(this.movie).subscribe(()  => {
-            var instruction = this.router.generate(['/Movies']);
-            this.router.navigateByInstruction(instruction);
-        });
-    }
-    isControlValid(cName:string,form:ControlGroup) {
-        var isValid=true;
-        if(this.movieForm.controls && this.movieForm.controls[cName]){
-            isValid=this.movieForm.controls[cName].valid;
-        }
-        return isValid;
-    }
-    getRangeNumberValidator(min, max) {
-        return function (c:Control):any {
-            if(c.value){
-                var val = parseInt(c.value);
-                //it's a number ?
-                if (isNaN(val)) {
-                    return {
-                        number: true
-                    }
-                }
-                //it's higher than min
-                if (val < min) {
-                    return {
-                        min: true
-                    }
-                }
-                //it's lower than max
-                if (val > max) {
-                    return {
-                        max: true
-                    }
-                }
-            }
-            // Null means valid
-            return null
-        };
+        this.http.put('api/movies', JSON.stringify(this.movie),{headers: new Headers({'Content-Type': 'application/json'})})
+            .subscribe(()=> {
+                this.router.navigate(['Movies']);
+            });
     }
 }
