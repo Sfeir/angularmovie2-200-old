7f42a1b3da5ee4474c654e2ac623ebe485714ab2
cbalit <dou4872>
2015-12-29 15:49:25 +0100

    angular2 beta router solution

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,8 +1,7 @@
-import {bootstrap, bind, FORM_PROVIDERS} from 'angular2/angular2';
-import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
-import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import {MoviesService} from './services/MoviesService';
+import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {App} from './app/App';
 
-
-bootstrap(App,[ROUTER_PROVIDERS,HTTP_PROVIDERS,FORM_PROVIDERS, MoviesService,bind(LocationStrategy).toClass(HashLocationStrategy)]);
+bootstrap(App,[HTTP_PROVIDERS,ROUTER_PROVIDERS,provide(LocationStrategy,{useClass:HashLocationStrategy})]);
