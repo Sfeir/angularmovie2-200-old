46bda59e51fcfd365a08da3fe9bdd394a0894f46
Cyril Balit <cbalit@gmail.com>
2016-02-09 07:35:23 +0100

    work in progress

---
+++ app/js/movie_edit/EditMovie.ts
@@ -0,0 +1,28 @@
+import {Movies} from '../movie_core/Movies';
+
+interface MovieRouteParams {
+    id: string
+}
+
+class EditMovieController {
+    movie:Object;
+
+    constructor(private movieService:Movies, $routeParams:MovieRouteParams, public $location){
+        var movieId = $routeParams.id;
+        this.movieService.fetchOne(movieId).subscribe((movie)=>{
+            this.movie = movie;
+        });
+    }
+
+
+    updateMovie(movie){
+        this.movieService.update(movie)
+            .subscribe(()=>{
+                this.$location.path('/movies');
+            });
+    }
+}
+
+EditMovieController.$inject = ['Movie', '$routeParams', '$location'];
+
+export default EditMovieController;
