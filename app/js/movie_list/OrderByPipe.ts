2b5d89e29e1870342fd49fb59f41be64845fb736
cbalit <dou4872>
2016-02-16 13:15:50 +0100

    add reverse in sort pipe

--- app/js/movie_list/OrderByPipe.ts
+++ app/js/movie_list/OrderByPipe.ts
@@ -3,12 +3,13 @@ import {Pipe} from 'angular2/core';
 export default class OrderByPipe {
     transform(input:Object[], args:string[]): Object[] {
         if (input) {
-            let property = args[0];
+            let property= args[0];
+            let reverseVal= args[1] ? 1:-1;
             return input.slice().sort((a, b) => {
                 if (a[property] < b[property]) {
-                    return -1;
+                    return -1*reverseVal;
                 } else if (b[property] < a[property]) {
-                    return 1;
+                    return 1*reverseVal;
                 } else {
                     return 0;
                 }
