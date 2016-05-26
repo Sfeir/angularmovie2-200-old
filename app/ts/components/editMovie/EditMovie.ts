d4af805c3b89fcd30b775ec8f4ee6a092744903a
Wassim CHEGHAM <github@wassimchegham.com>
2016-05-26 15:49:58 +0200

    chore(component): update EditMovie component

--- app/ts/components/editMovie/EditMovie.ts
+++ app/ts/components/editMovie/EditMovie.ts
@@ -1,6 +1,6 @@
-import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES, FormBuilder, Validators, ControlGroup, Control} from 'angular2/common';
-import {Router,RouterLink,RouteParams} from 'angular2/router'
+import {Component} from '@angular/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, FormBuilder, Validators, ControlGroup, Control} from '@angular/common';
+import {Router,RouterLink,RouteParams} from '@angular/router-deprecated';
 import {MoviesService} from '../../services/MoviesService';
 
 
