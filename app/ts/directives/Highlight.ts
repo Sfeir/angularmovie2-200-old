19979dd893ab4bd84a1486c2073199a693f6c645
Wassim CHEGHAM <github@wassimchegham.com>
2016-05-26 15:50:38 +0200

    chore(directive): update Highlight component

--- app/ts/directives/Highlight.ts
+++ app/ts/directives/Highlight.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef,EventEmitter,Inject} from 'angular2/core';
+import {Directive, ElementRef,EventEmitter,Inject} from '@angular/core';
 
 @Directive({
     selector: '[highlight]',
