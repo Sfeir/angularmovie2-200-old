b4ae1df4a81085b71c0856c800603b8bb33e3833
cbalit <dou4872>
2016-05-30 11:12:43 +0200

    new config for angular 2 rc1
    Conflicts:
    	app/ts/bootstrapHome.ts

---
+++ app/system-config.js
@@ -0,0 +1,31 @@
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router-deprecated',
+    '@angular/router-deprecated',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+
+    // Thirdparty barrels.
+    'rxjs'
+];
+var cliSystemConfigPackages = {
+    'ts': {
+        defaultExtension: 'js'
+    }
+};
+barrels.forEach(function(barrelName){
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+System.config({
+    map: {
+        '@angular': '@angular',
+        'rxjs': 'rxjs',
+        'main': 'ts/bootstrapHome.js',
+        'app': 'app'
+    },
+    packages: cliSystemConfigPackages
+});
