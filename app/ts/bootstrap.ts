6667487e0beac04c03fd79498615a56fe0b7ca2c
cbalit <dou4872>
2016-05-30 14:15:14 +0200

    RC1:MovieService component

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -4,8 +4,10 @@ import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {LocationStrategy,HashLocationStrategy} from '@angular/common';
 import {App} from './app/App';
+import {MoviesService} from './services/MoviesService';
 
 bootstrap(App,[
+    MoviesService,
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
     provide(LocationStrategy,{useClass:HashLocationStrategy})
