e7a1319ed2a59c997ebbca62c0ea203e3653e32e
cbalit <dou4872>
2016-05-30 11:53:06 +0200

    RC1:update conf for HTT

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {MoviesComponent} from './components/movies/Movies';
 
-bootstrap(MoviesComponent);
+bootstrap(MoviesComponent,[HTTP_PROVIDERS]);
+
