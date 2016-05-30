7c4bcb9686cbdf5e2f21805d485ba38bf4dd8e1e
cbalit <dou4872>
2016-05-30 11:38:56 +0200

    RC1: update movie form component

--- app/ts/components/movieForm/MovieFormComponent.ts
+++ app/ts/components/movieForm/MovieFormComponent.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter} from 'angular2/core';
-import {FORM_DIRECTIVES} from 'angular2/common';
+import {Component, EventEmitter} from '@angular/core';
+import {FORM_DIRECTIVES} from '@angular/common';
 
 @Component({
     selector: 'movie-form',
