d5593912e2a4b152c10ef9f8ea6369d0ec4e89ef
cbalit <dou4872>
2016-05-30 13:48:13 +0200

    RC1:edit component

--- app/ts/components/editMovie/EditMovie.ts
+++ app/ts/components/editMovie/EditMovie.ts
@@ -1,7 +1,7 @@
-import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
-import {Router,RouterLink,RouteParams} from 'angular2/router'
-import {Http,Headers} from 'angular2/http';
+import {Component} from '@angular/core';
+import {FORM_DIRECTIVES} from '@angular/common';
+import {Router,RouterLink,RouteParams} from '@angular/router-deprecated'
+import {Http,Headers} from '@angular/http';
 
 @Component({
     selector: 'edit-movie',
