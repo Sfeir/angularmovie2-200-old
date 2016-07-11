d88b65e6bd16ed07b937013d74816160b613180b
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-11 16:49:30 +0200

    update: use new router

--- app/ts/app/App.ts
+++ app/ts/app/App.ts
@@ -1,20 +1,9 @@
 import {Component} from '@angular/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated'
-import {HomeComponent} from '../components/home/Home'
-import {MoviesComponent} from '../components/movies/Movies'
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
     selector: 'movies-app',
     templateUrl:'ts/app/app.html',
-    directives:[ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES]
 })
-@RouteConfig([
-    { path: '/', name: 'Home', component: HomeComponent },
-    { path: '/home', name: 'Home', component: HomeComponent },
-    { path: '/movies', name: 'Movies', component: MoviesComponent }
-])
-export class App {
-    constructor() {
-
-    }
-}
+export class App {}
--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,12 +1,12 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {provide} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
-import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
-import {LocationStrategy,HashLocationStrategy} from '@angular/common';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {App} from './app/App';
+import {APP_ROUTER_PROVIDERS} from './app.routes';
 
 bootstrap(App,[
     HTTP_PROVIDERS,
-    ROUTER_PROVIDERS,
-    provide(LocationStrategy,{useClass:HashLocationStrategy})
+    APP_ROUTER_PROVIDERS,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
 ]);
