22fb006a7368b5cfcd526852ef26825e6fc65532
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-11 16:49:30 +0200

    use new router

--- app/ts/bootstrap.ts
+++ app/ts/bootstrap.ts
@@ -1,14 +1,12 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {provide} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
-import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
-import {LocationStrategy,HashLocationStrategy} from '@angular/common';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {App} from './app/App';
-import {MoviesService} from './services/MoviesService';
+import {APP_ROUTER_PROVIDERS} from './app.routes';
 
 bootstrap(App,[
-  MoviesService,
-  HTTP_PROVIDERS,
-  ROUTER_PROVIDERS,
-  provide(LocationStrategy,{useClass:HashLocationStrategy})
+    HTTP_PROVIDERS,
+    APP_ROUTER_PROVIDERS,
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
 ]);
