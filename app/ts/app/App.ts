4999d5edeae3fb4298d13aa2e8cdb0177879af84
Wassim CHEGHAM <github@wassimchegham.com>
2016-07-13 12:13:08 +0200

    use new router

--- app/ts/app/App.ts
+++ app/ts/app/App.ts
@@ -1,18 +1,9 @@
 import {Component} from '@angular/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
-import {HomeComponent} from '../components/home/Home';
-import {EditMovieComponent} from '../components/editMovie/EditMovie';
-import {MoviesComponent} from '../components/movies/Movies';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
     selector: 'movies-app',
     templateUrl:'ts/app/app.html',
-    directives:[ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES]
 })
-@RouteConfig([
-  { path: '/', name: 'Home', component: HomeComponent },
-  { path: '/home', name: 'Home', component: HomeComponent },
-  { path: '/movies', name: 'Movies', component: MoviesComponent },
-  { path: '/movies/edit/:id', name:'EditMovie', component: EditMovieComponent }
-])
 export class App {}
